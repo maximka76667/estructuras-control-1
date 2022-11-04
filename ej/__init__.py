69054bd46a32c95afd3756c8fa25d4a5992557f8
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-11-04 11:53:52 +0100

    Update __init__.py

---
+++ ej/__init__.py
@@ -0,0 +1,22 @@
+from random import randrange
+
+def minmax(a, b):
+    if a < b:
+        return [b, a]
+    return [a, b]
+
+def calcularPasos(canalA, canalB):
+    dif = canalA - canalB
+    return NUM_CANALES - canalA + canalB if dif > NUM_CANALES / 2 else dif
+
+NUM_CANALES = 99
+
+for i in range(3):
+    canales = [randrange(1, NUM_CANALES), randrange(1, NUM_CANALES)]
+    
+    print(canales)
+    
+    [canalA, canalB] = minmax(canales[0], canales[1])
+    
+    
+    print(calcularPasos(canalA, canalB))
